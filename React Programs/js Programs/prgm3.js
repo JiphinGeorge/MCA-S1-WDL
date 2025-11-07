c526dab7d1bff94faeba10bf90479a4053e0e11d
JiphinGeorge <jiphingeorge80.com>
2025-11-07 11:20:50 +0530

    deleted the js programs from the lab

--- React Programs/js Programs/prgm3.js
+++
@@ -1,9 +0,0 @@
-const con = require('./Ex_db.js');
-
-const sql = "INSERT INTO students (rollno, name) VALUES (6, 'Rahul')";
-
-con.query(sql, (err, result) => {
-  if (err) throw err;
-  console.log('Record inserted successfully!');
-  con.end();
-});
